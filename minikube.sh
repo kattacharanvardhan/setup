2c315c67191237c4ca2412a63f997b4f8d50e71a
Charan <163161553+kattacharanvardhan@users.noreply.github.com>
2026-07-20 17:24:19 +0530

    Update and rename K8s to minikube.sh

---
+++ minikube.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+sudo apt update -y
+sudo apt upgrade -y
+
+sudo apt install -y curl wget apt-transport-https ca-certificates
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+sudo systemctl enable docker
+sudo systemctl start docker
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+minikube version
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+
+echo "$(cat kubectl.sha256) kubectl" | sha256sum --check
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+kubectl version --client
+
+sudo minikube start --driver=docker --force
+
+sudo minikube status
+
+kubectl get nodes
