585b09a49f52d6b3278564f330545a4f7ed8d38b
Charan <163161553+kattacharanvardhan@users.noreply.github.com>
2026-09-04 11:28:06 +0530

    Update pythoncode.py

--- pythoncode.py
+++ pythoncode.py
@@ -8,3 +8,10 @@ for i in range(1,6,1):
   for j in range(1,6,1):
     print(i, end = " ")
 
+# print shape shape output with *
+for i in range(1,6,1):
+  for j in range(1,6,1):
+    print("*" , end = " ")
+  print("\r")
+
+# print right angle triangle 
