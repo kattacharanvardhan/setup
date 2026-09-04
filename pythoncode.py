3b09c082962e7c66691f6b800c120978ff79e627
Charan <163161553+kattacharanvardhan@users.noreply.github.com>
2026-09-04 11:53:40 +0530

    Implement left angle triangle printing in Python
    Added code to print left angle triangles using numbers and asterisks.

--- pythoncode.py
+++ pythoncode.py
@@ -26,3 +26,18 @@ for i in range(1,6,1):
     print("*", end = " ")
   print("/r")
 
+# print left angle triangle with number
+for i in range(1,6,1):
+  for k in range(1,6-i,1):
+    print(end = " ")
+  for j in range(1,i,1):
+    print(i, end = " ")
+  print("\r")
+
+# print left angle triangle with *
+for i in range(1,6,1):
+  for k in range(1,6-i,1):
+    print(end = " ")
+  for j in range(1,i,1):
+    print("*", end = " ")
+  print("\r")
