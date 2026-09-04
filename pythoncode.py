fc301b1cddee4e352d94a1cf817a70b25cd51488
Charan <163161553+kattacharanvardhan@users.noreply.github.com>
2026-09-04 11:18:52 +0530

    Create pythoncode.py

---
+++ pythoncode.py
@@ -0,0 +1,10 @@
+# print vertical output [range(start,end,skip)]
+for i in range(1,6,1):
+  for j in range(1,6,1):
+    print(i)
+
+# print convert vertical output to horizontal output
+for i in range(1,6,1):
+  for j in range(1,6,1):
+    print(i, end = " ")
+
